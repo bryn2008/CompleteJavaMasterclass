46d65861ff393c895dd60458bbe774cd16fdc566
Bryn Lloyd <brynla.lloyd@gmail.com>
2017-10-09 21:44:38 +0100

    added mobile phone class

--- src/main/java/uk/co/cjmc/section_10/interfaces/Main.java
+++ src/main/java/uk/co/cjmc/section_10/interfaces/Main.java
@@ -3,11 +3,15 @@ package uk.co.cjmc.section_10.interfaces;
 public class Main {
 
     public static void main(String[] args) {
-        ITelephone timsPhone;
-        timsPhone = new DeskPhone(123456);
-        timsPhone.powerOn();
-        timsPhone.callPhone(123456);
-        timsPhone.answer();
+        ITelephone brynsPhone;
+//        brynsPhone = new DeskPhone(123456);
+//        brynsPhone.powerOn();
+//        brynsPhone.callPhone(123456);
+//        brynsPhone.answer();
+
+        brynsPhone = new MobilePhone(246895);
+        brynsPhone.callPhone(246895);
+        brynsPhone.answer();
 
     }
 }
