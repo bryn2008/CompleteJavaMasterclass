0ed22676d2151d3b4fc1708f456e9b0a3d0b2a2a
bryn-lloyd <bryn_1@hotmail.com>
2018-02-27 16:14:53 +0000

    re-train in java

--- src/main/java/uk/co/cjmc/section3/Hello.java
+++ src/main/java/uk/co/cjmc/section3/Hello.java
@@ -1,15 +1,10 @@
-/**
- * Hello.java
- * (C) Crown Copyright 2017, the Met Office.
- */
 package uk.co.cjmc.section3;
 
-/**
- * Created by bryn.lloyd on 09/10/2017.
- */
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Bryn!");
+
     }
+
 }
